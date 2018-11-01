2edaa6f08e10bb63f3ef37ca618814935adddd0a
dwmoeller1 <dwmoeller1@aol.com>
2018-11-01 18:50:26 -0500

    Walkthrough week1 Perimeter program

---
+++ Perimeter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user input height
+            Console.WriteLine("Enter height:");
+            string heightInput = Console.ReadLine();
+            //user input width
+            Console.WriteLine("Enter width:");
+            string widthInput = Console.ReadLine();
+            //calculate perimeter
+            int perimeter = CalculatePerimeter(heightInput, widthInput);
+            //print out perimeter
+            Console.WriteLine(perimeter);
+            Console.ReadLine();            
+        }
+
+        private static int CalculatePerimeter(string heightInput, string widthInput)
+        {
+            int perimeter =0;
+            bool didItWork = int.TryParse(heightInput, out int height);
+            int width = int.Parse(widthInput);
+            perimeter = 2 * height + 2 * width;
+            return perimeter;
+        }
+    }
+}
