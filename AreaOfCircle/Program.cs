3c86eda282827eca64c6d00d7e92cb9d2079a829
dwmoeller1 <dwmoeller1@aol.com>
2018-11-05 16:46:19 -0600

    Solution for studio 1

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radiusInput = GetRadiusInput();
+            double area = CalculateArea(radiusInput);
+            ReturnArea(area);
+        }
+
+        private static void ReturnArea(double area)
+        {
+            Console.WriteLine($"The area is: {area}");
+        }
+
+        private static double CalculateArea(int radius)
+        {
+            return Math.Round(Math.PI * Math.Pow(radius, 2), 3);
+        }
+
+        private static int GetRadiusInput()
+        {
+            int radius;
+            Console.WriteLine("Enter radius:");
+            while (!ValidateInput(Console.ReadLine(), out radius){
+                Console.WriteLine("Please enter a integer.");
+                Console.WriteLine("Enter radius:");
+            }
+            return radius;
+        }
+
+        private static bool ValidateInput(string input, out int output)
+        {
+            return int.TryParse(input, out output);
+        }
+    }
+}
