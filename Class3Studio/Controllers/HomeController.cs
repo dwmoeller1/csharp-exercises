d58fc5f431c5ffeb7bce8438fb0c56676bc822a4
dwmoeller1 <dwmoeller1@aol.com>
2018-11-08 20:22:42 -0600

    Studio3 with string options

---
+++ Class3Studio/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Class3Studio.Models;
+using System.IO;
+
+namespace Class3Studio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Welcome()
+        {
+            string htmlString = System.IO.File.ReadAllText("welcome_html.txt");
+            return Content(htmlString, "text/html");
+        }
+
+        [HttpPost]
+        public IActionResult Welcome(string name, string language)
+        {
+            Dictionary<string, string> languageDict = GreetingDictionary();
+            string greeting = languageDict[language];
+            return Content($"{greeting}, {name}!");
+        }
+
+        private Dictionary<string, string> GreetingDictionary()
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("English", "Hello");
+            dict.Add("Russian", "Priviet");
+            dict.Add("French", "Bonjour");
+            dict.Add("Spanish", "Hola");
+            dict.Add("Canadian", "Hi, eh?");
+            return dict;
+        }
+    }
+}
---
+++ Class3Studio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Class3Studio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
